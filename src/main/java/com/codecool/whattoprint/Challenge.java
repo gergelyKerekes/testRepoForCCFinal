bcc156e528c31f5ccc4ec6c1e178c93fd9693d0d
Gergely Kerekes <kerekes.gergely83@gmail.com>
2021-03-23 19:40:24 +0100

    method impl

--- src/main/java/com/codecool/whattoprint/Challenge.java
+++ src/main/java/com/codecool/whattoprint/Challenge.java
@@ -1,7 +1,22 @@
 package com.codecool.whattoprint;
 
+import java.util.Arrays;
+
 class Challenge {
+
     public static String decideAndSay(String[] words, int number) {
-        return words[0];
+        String ret = "";
+
+        if (words.length > 1) {
+            if (number % 3 == 0 && number % 7 == 0) {
+                ret = words[0] + words[1];
+            } else if (number % 3 == 0) {
+                ret = words[0];
+            } else if (number % 7 == 0) {
+                ret = words[1];
+            }
+        }
+        return ret;
     }
 }
+
